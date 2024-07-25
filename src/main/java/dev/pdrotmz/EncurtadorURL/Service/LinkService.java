c09ac1eac32a84c689ace666970cffe2e7eef203
pdrotmz <pedrotomaz0210@gmail.com>
2024-07-25 09:52:07 -0300

    feat(service): add LinkService class

---
+++ src/main/java/dev/pdrotmz/EncurtadorURL/Service/LinkService.java
@@ -0,0 +1,43 @@
+package dev.pdrotmz.EncurtadorURL.Service;
+
+
+import dev.pdrotmz.EncurtadorURL.Model.Link;
+import dev.pdrotmz.EncurtadorURL.Repository.LinkRepository;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class LinkService {
+    private LinkRepository repository;
+
+    public LinkService(LinkRepository repository) {
+        this.repository = repository;
+    }
+
+    // Gerando uma url aleatória
+    //TODO: REFATORAR PARA INCLUIR PARTE DA URL ORIGINAL NO NOSSO ALGORITMO DE GERAÇÃO DE URL
+    public String gerarUrlAleatoria(){
+        return RandomStringUtils.randomAlphanumeric(5, 10);
+    }
+
+    public Link encurtarUrl(String urlOriginal) {
+        Link link = new Link();
+        link.setUrlLonga(urlOriginal);
+        link.setUrlEncurtada(gerarUrlAleatoria());
+        link.setUrlCriadaEm(LocalDateTime.now());
+        link.setUrlQrCode("QR CODE INDISPONÍVEL NO MOMENTO");
+
+        return repository.save(link);
+    }
+
+    public Link obterUrlOriginal(String urlEncurtada) {
+        try {
+            return repository.findByUrlOriginal(urlEncurtada);
+        } catch (Exception erro) {
+            throw new RuntimeException("Url não existe no registro", erro);
+        }
+
+    }
+}
