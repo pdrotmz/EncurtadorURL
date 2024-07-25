6e67648d51eb95943d1543a0607c20ff0147532e
pdrotmz <pedrotomaz0210@gmail.com>
2024-07-25 07:32:41 -0300

    feat(Repository): LinkRepository interface add

---
+++ src/main/java/dev/pdrotmz/EncurtadorURL/Repository/LinkRepository.java
@@ -0,0 +1,8 @@
+package dev.pdrotmz.EncurtadorURL.Repository;
+
+import dev.pdrotmz.EncurtadorURL.Model.Link;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LinkRepository extends JpaRepository<Link, Integer> {
+
+}
