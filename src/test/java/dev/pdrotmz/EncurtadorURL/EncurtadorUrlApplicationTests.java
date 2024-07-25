572aed72232bce0fb21d99ce69521fa1d0209c65
pdrotmz <pedrotomaz0210@gmail.com>
2024-07-25 07:17:56 -0300

    Initial commit

---
+++ src/test/java/dev/pdrotmz/EncurtadorURL/EncurtadorUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pdrotmz.EncurtadorURL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtadorUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
